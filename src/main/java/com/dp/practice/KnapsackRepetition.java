ca2a72b5589e50d36d1a50cca244ecde4a756a4d
anupam-veedna <anupam@veedna.com>
2024-07-28 18:08:29 +0530

    knapsack repetition matrix method added

--- src/main/java/com/dp/practice/KnapsackRepetition.java
+++ src/main/java/com/dp/practice/KnapsackRepetition.java
@@ -5,23 +5,25 @@ import java.util.Map;
 
 /**
  * check matrix approach
- * i -> items weight in sorted order
- * j -> total weight
- * T[i][j] = MAX(T[i-1][j] , V[i] + T[i][j-i]) if(j-i > 0) else T[i-1][j]
+ * i -> items weight [include weight till index i]
+ * j -> total weight (capacity)
+ * T[i][j] = MAX(T[i-1][j] , V[i] + T[i][j-wt[i]]) if(j-wt[i] >= 0) else T[i-1][j]
  *
  */
-public class KnapsackRepetion {
+public class KnapsackRepetition {
 
     // Time complexity O(n log n)
     public static void main(String[] args) {
         int[] wt = {10, 40, 20, 30};
-        int[] val = {60, 40, 100, 120};
+        int[] val = {6, 40, 100, 120};
         int capacity = 50;
 
         int maxValue = getMaxValue(wt, val, capacity);
         System.out.println("Maximum value we can obtain = " + maxValue);
 
         System.out.println("Maximum value we can obtain = " + getMaxValueDP(wt, val, capacity));
+
+        getMaxValueDpMatrix(wt, val, capacity);
     }
 
     // // function to get maximum value
@@ -59,6 +61,30 @@ public class KnapsackRepetion {
         dpCapacity.put(capacity, totalVal);
         return totalVal;
 
+    }
+
+    // space and time complexity : O(w*n)
+    private static void getMaxValueDpMatrix(int[] wt, int[] val, int capacity) {
+
+        int [][] dp = new int[wt.length][capacity+1];
+
+        for(int i=0; i < wt.length; i++) {
+            for(int j=0; j <= capacity; j++) {
+                if(j-wt[i] >= 0 ) {  // current element can be included
+                    dp[i][j] = Math.max(i>0 ? dp[i-1][j] : 0, val[i] + dp[i][j-wt[i]]);
+                } else {  // current element can not be included
+                    dp[i][j] = i>0 ? dp[i-1][j] : 0;
+                }
+            }
+        }
+
+        // Print all elements of the 2D array
+        for (int i = 0; i < dp.length; i++) { // Iterate over rows
+            for (int j = 0; j < dp[i].length; j++) { // Iterate over columns
+                System.out.print(dp[i][j] + " "); // Print element with a space
+            }
+            System.out.println(); // Move to the next line after each row
+        }
     }
 
 
--- src/main/java/com/dp/practice/MinimumJumpToReachLast.java
+++ src/main/java/com/dp/practice/MinimumJumpToReachLast.java
@@ -23,7 +23,7 @@ public class MinimumJumpToReachLast {
 			jumpArr[i] = Integer.MAX_VALUE;
 		}
 		jumpArr[0] = 0;
-		for (int i = 0; i < arr.length; i++) {
+		for (int i = 1; i < arr.length; i++) {
 			for (int j = 0; j < i; j++) {
 				if (arr[j] >= i - j && jumpArr[i] > jumpArr[j] + 1) { // jump possible and results optimal
 					jumpArr[i] = jumpArr[j] + 1;
