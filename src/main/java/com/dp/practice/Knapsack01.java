a1aa9d822439ccb05f0875ed5005f0aca1668790
anupam-veedna <anupam@veedna.com>
2024-07-28 21:58:23 +0530

    0/1 knapsack  metrix approach added

--- src/main/java/com/dp/practice/Knapsack01.java
+++ src/main/java/com/dp/practice/Knapsack01.java
@@ -2,58 +2,67 @@ package com.dp.practice;
 
 /*
  * check youtube : https://www.youtube.com/watch?v=8LusJS5-AGo
- * 
- * 
- * row i (i number of items) 
- * 
- * and 
- * 
+ *
+ *
+ * row i (i number of items)
+ *
+ * and
+ *
  * column j+1 [final weight j] [0,1,2,...j]
- * 
+ *
  * formula
- * 
+ *
  * if(j < wt[i]){
  * 	T[i][j] = T[i-1][j];
  * } else {
  * 	T[i][j] = MAX (T[i-1][j] , v[i] + T[i-1][j-wt[i]])
  * }
- * 
- * 
- * 
+ *
+ *
+ *
  */
 public class Knapsack01 {
 
-	private static int MaxKnapsack01Recursive(int W, int[] val, int[] wt, int n) {
-		if (W == 0 || n == 0) {
-			return 0;
-		}
-		if (wt[wt.length - 1] > W) {
-			return MaxKnapsack01Recursive(W, val, wt, n - 1);
-		} else {
-			return Math.max(val[val.length - 1] + MaxKnapsack01Recursive(W - wt[wt.length - 1], val, wt, n - 1),
-					MaxKnapsack01Recursive(W, val, wt, n - 1));
-		}
-	}
+    private static int MaxKnapsack01Recursive(int W, int[] val, int[] wt, int n) {
+        if (W == 0 || n == 0) {
+            return 0;
+        }
+        if (wt[wt.length - 1] > W) {
+            return MaxKnapsack01Recursive(W, val, wt, n - 1);
+        } else {
+            return Math.max(val[val.length - 1] + MaxKnapsack01Recursive(W - wt[wt.length - 1], val, wt, n - 1),
+                    MaxKnapsack01Recursive(W, val, wt, n - 1));
+        }
+    }
 
-	private static int MaxKnapsack01DP(int[] val, int[] wt) {
-		int[][] dp = new int[val.length + 1][wt.length + 1];
+    private static int MaxKnapsack01DP(int[] val, int[] wt) {
+        int[][] dp = new int[val.length + 1][wt.length + 1];
 
-		for (int i = 0; i <= val.length; i++) {
-			for (int w = 0; w <= wt.length; w++) {
-				if (i == 0 || w == 0) {
-					dp[i][w] = 0;
-				} else if (wt[i - 1] < w) {
-					dp[i][w] = Math.max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w]);
-				} else {
-					dp[i][w] = dp[i - 1][w];
-				}
-			}
-		}
+        for (int i = 0; i <= val.length; i++) {
+            for (int w = 0; w <= wt.length; w++) {
+                if (i == 0 || w == 0) {
+                    dp[i][w] = 0;
+                } else if (wt[i - 1] < w) {
+                    dp[i][w] = Math.max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w]);
+                } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
+            }
+        }
 
-		return dp[val.length][wt.length];
-	}
+        return dp[val.length][wt.length];
+    }
 
-	private static void MaxKnapsack01DPMatrix() {
-
-	}
+    private static void MaxKnapsack01DPMatrix(int[] val, int[] wt, int capacity) {
+        int[][] dp = new int[wt.length][capacity + 1];
+        for (int i = 0; i < wt.length; i++) {
+            for (int j = 1; j <= capacity; j++) {
+                if (j - wt[i] >= 0) { // ith item can be included
+                    dp[i][j] = Math.max(i > 0 ? dp[i - 1][j] : 0, val[i] + i > 0 ? dp[i - 1][j - wt[i]] : 0);
+                } else { // ith item can not be included
+                    dp[i][j] = i > 0 ? dp[i - 1][j] : 0;
+                }
+            }
+        }
+    }
 }
