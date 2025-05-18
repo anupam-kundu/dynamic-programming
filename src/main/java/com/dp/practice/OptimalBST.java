158ce1cef4dcb63955bd0c3a8ef4331993195704
anupam-kundu <anupamkundu3@gmail.com>
2025-05-18 11:50:20 +0530

    added files

--- src/main/java/com/dp/practice/OptimalBST.java
+++ src/main/java/com/dp/practice/OptimalBST.java
@@ -1,4 +1,11 @@
 package com.dp.practice;
 
+/**
+ * Optimal BST
+ * where frequency of each node given
+ *
+ * dp[i][j] = sum(freq(i-j)) + min(s->left, s->right)
+ *
+ */
 public class OptimalBST {
 }
